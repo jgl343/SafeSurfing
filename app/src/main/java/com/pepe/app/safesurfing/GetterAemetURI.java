b2d71dc2638f606a02234e20002584a11841fe06
JOSE ANTONIO GONZALEZ LOPEZ <0011445@insags.com>
2016-09-15 09:28:48 +0200

    Initial commit

---
+++ app/src/main/java/com/pepe/app/safesurfing/GetterAemetURI.java
@@ -0,0 +1,9 @@
+package com.pepe.app.safesurfing;
+
+/**
+ * Created by 0011445 on 14/09/2016.
+ */
+public interface GetterAemetURI {
+
+    public String getAemetURI(String provincia, String municipio);
+}
